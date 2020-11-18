f2080d62826d04e1343c39d5f58e53277b803a8a
Efren Serrano <omar.serrano90@gmail.com>
2020-11-17 22:34:00 -0600

    changes for v5

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hola Lili y Daniel</h1>
-        <h2>Hola Lili y Daniel</h2>
+        <h2>Les agradezco mucho que hayan dedicado una hora de su tiempo a conocerme, 
+         en ese momento estaba muy emocionado me senti privilegiado
+         y un poco corto en experiencia de desarrollo de Software y Metodologias Agiles. 
+         Por eso, desde ese dia decidi estudiar y practicar. 
+         Esta pagina es una app React, en JavaScript, usando un repositorio GITHUB y utilizando AWS</h2>
+        <h3>Yo creo que todo lo que puedes visualizar es posible, pero eso no significa que sea facil, la clave para mi es el compromiso a la excelencia,
+         o como dicen los principios de Grupo Modelo: complacencia cero</h3>
+        <h4>Si quiero ser un excelente Product Manager, ademas de captar bien las necesidades del consumidor, tengo que hablar a la prefeccion
+         el lenguaje tecnico, por eso me di a la tarea de re-aprender a programar... no me queda de otra; no hay atajos.</h4>
+        <h5>Quise que en esta app tambien encontraran un ejemplo de autenticacion y de base de datos en el back end, pero honestamente
+        aprender eso me llevo mas tiempo de lo esperado, pero estoy seguro de que pronto lo aprendere a hacer, pues deseo estar a un nivel de excelencia tecnico</h5>
+        <h6>Gracias por tomarse un tiempo y leerme! Disculpen las faltas de acentuacion, no es por descuido.</h6>
       </header>
     </div>
   );
