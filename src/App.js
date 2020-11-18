a1edb5ef21f348c08159418b1749c0e8e8c39a8b
Efren Serrano <omar.serrano90@gmail.com>
2020-11-17 22:44:19 -0600

    changes for v6

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hola Lili y Daniel</h1>
-        <h2>Les agradezco mucho que hayan dedicado una hora de su tiempo a conocerme, 
+        <h3>Les agradezco mucho que hayan dedicado una hora de su tiempo a conocerme, 
          en ese momento estaba muy emocionado me senti privilegiado
-         y un poco corto en experiencia de desarrollo de Software y Metodologias Agiles. 
+         y me di cuenta que puedo incrementar mis habilidades en desarrollo de Software y Metodologias Agiles. 
          Por eso, desde ese dia decidi estudiar y practicar. 
-         Esta pagina es una app React, en JavaScript, usando un repositorio GITHUB y utilizando AWS</h2>
+         Esta pagina es una app React, en JavaScript, usando un repositorio GITHUB y utilizando AWS.</h3>
         <h3>Yo creo que todo lo que puedes visualizar es posible, pero eso no significa que sea facil, la clave para mi es el compromiso a la excelencia,
          o como dicen los principios de Grupo Modelo: complacencia cero</h3>
-        <h4>Si quiero ser un excelente Product Manager, ademas de captar bien las necesidades del consumidor, tengo que hablar a la prefeccion
-         el lenguaje tecnico, por eso me di a la tarea de re-aprender a programar... no me queda de otra; no hay atajos.</h4>
-        <h5>Quise que en esta app tambien encontraran un ejemplo de autenticacion y de base de datos en el back end, pero honestamente
-        aprender eso me llevo mas tiempo de lo esperado, pero estoy seguro de que pronto lo aprendere a hacer, pues deseo estar a un nivel de excelencia tecnico</h5>
-        <h6>Gracias por tomarse un tiempo y leerme! Disculpen las faltas de acentuacion, no es por descuido.</h6>
+        <h3>Si quiero ser un excelente Product Manager, ademas de captar bien las necesidades del consumidor, tengo que hablar a la prefeccion
+         el lenguaje tecnico, por eso me di a la tarea de re-aprender a programar... no me queda de otra; no hay atajos.</h3>
+        <h3>Mi siguiente paso sera hacer una pagina mas dinamica, con autenticacion y de base de datos en el back end para captar informacion de mis visitantes,
+        aprender eso me llevara mas tiempo, pero estoy seguro de que pronto lo aprendere a hacer pues deseo estar a un nivel digno de Grupo Modelo.</h3>
+        <h3>En cuanto a Metodologias Agiles, este 21 y 28 de noviembre tendre un curso por el PMI en metodologias agiles de 8 horas diarias y si necesitara mas conocimiento buscare desarrollarlo.</h3>
+        <h3>Gracias por tomarse un tiempo y leerme! Ojala aun este a tiempo de poder ser parte de su equipo. Disculpen las faltas de acentuacion, no es por descuido, tengo un bug con los caracteres especiales que no he corregido y prefiero ponerlo sin acento para que sea legible.</h3>
       </header>
     </div>
   );
